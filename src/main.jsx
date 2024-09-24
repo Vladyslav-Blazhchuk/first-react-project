6b5ad4b84ab5c66166be9879e7e28f32e410d9a4
Vladyslav Blazhchuk <mr.goodlike1@gmail.com>
2024-09-24 21:14:11 +0300

    initial

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux'
 import { store } from './store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
+  <Provider store={store} basename="/first-react-project/">
     <RouterProvider router={router} />
   </Provider>
 )
